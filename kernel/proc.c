cc72249fa838d655c0cad75f80ee0f6ca21a2706
lian.tang <tl2039462144@.163.com>
2024-08-12 22:17:48 +0800

    syscall

--- kernel/proc.c
+++ kernel/proc.c
@@ -291,6 +291,8 @@ fork(void)
 
   safestrcpy(np->name, p->name, sizeof(p->name));
 
+  //将trace_mask拷贝到子进程
+  np->trace_mask = p->trace_mask;
   pid = np->pid;
 
   np->state = RUNNABLE;
@@ -691,5 +693,15 @@ procdump(void)
       state = "???";
     printf("%d %s %s", p->pid, state, p->name);
     printf("\n");
+  }
+}
+void
+procnum(uint64 *dst)
+{
+  *dst = 0;
+  struct proc *p;
+  for (p = proc; p < &proc[NPROC]; p++) {
+    if (p->state != UNUSED)
+      (*dst)++;
   }
 }
